e223693bdc0c319d1d11999bcc13e05fccd253fc
MrJackSpade <loffredo.ap@gmail.com>
2021-02-10 23:09:00 -0700

    init

---
+++ BiliBili-dl/PlayerInfo.cs
@@ -0,0 +1,186 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace BiliBili_dl
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class SegmentBase
+    {
+        [JsonProperty("Initialization")]
+        public string Initialization { get; set; }
+
+        [JsonProperty("indexRange")]
+        public string IndexRange { get; set; }
+    }
+
+    public class SegmentBase2
+    {
+        [JsonProperty("initialization")]
+        public string Initialization { get; set; }
+
+        [JsonProperty("index_range")]
+        public string IndexRange { get; set; }
+    }
+
+    public class Media
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("baseUrl")]
+        public string BaseUrl { get; set; }
+
+        [JsonProperty("base_url")]
+        public string Base_Url { get; set; }
+
+        [JsonProperty("backupUrl")]
+        public object BackupUrl { get; set; }
+
+        [JsonProperty("backup_url")]
+        public object Backup_Url { get; set; }
+
+        [JsonProperty("bandwidth")]
+        public int Bandwidth { get; set; }
+
+        [JsonProperty("mimeType")]
+        public string MimeType { get; set; }
+
+        [JsonProperty("mime_type")]
+        public string Mime_Type { get; set; }
+
+        [JsonProperty("codecs")]
+        public string Codecs { get; set; }
+
+        [JsonProperty("width")]
+        public int Width { get; set; }
+
+        [JsonProperty("height")]
+        public int Height { get; set; }
+
+        [JsonProperty("frameRate")]
+        public string FrameRate { get; set; }
+
+        [JsonProperty("frame_rate")]
+        public string Frame_Rate { get; set; }
+
+        [JsonProperty("sar")]
+        public string Sar { get; set; }
+
+        [JsonProperty("startWithSap")]
+        public int StartWithSap { get; set; }
+
+        [JsonProperty("start_with_sap")]
+        public int Start_With_Sap { get; set; }
+
+        [JsonProperty("SegmentBase")]
+        public SegmentBase SegmentBase { get; set; }
+
+        [JsonProperty("segment_base")]
+        public SegmentBase Segment_Base { get; set; }
+
+        [JsonProperty("codecid")]
+        public int Codecid { get; set; }
+    }
+
+
+    public class Dash
+    {
+        [JsonProperty("duration")]
+        public int Duration { get; set; }
+
+        [JsonProperty("minBufferTime")]
+        public double MinBufferTime { get; set; }
+
+        [JsonProperty("min_buffer_time")]
+        public double Min_Buffer_Time { get; set; }
+
+        [JsonProperty("video")]
+        public List<Media> Video { get; set; }
+
+        [JsonProperty("audio")]
+        public List<Media> Audio { get; set; }
+    }
+
+    public class SupportFormat
+    {
+        [JsonProperty("quality")]
+        public int Quality { get; set; }
+
+        [JsonProperty("format")]
+        public string Format { get; set; }
+
+        [JsonProperty("new_description")]
+        public string NewDescription { get; set; }
+
+        [JsonProperty("display_desc")]
+        public string DisplayDesc { get; set; }
+
+        [JsonProperty("superscript")]
+        public string Superscript { get; set; }
+    }
+
+    public class Data
+    {
+        [JsonProperty("from")]
+        public string From { get; set; }
+
+        [JsonProperty("result")]
+        public string Result { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("quality")]
+        public int Quality { get; set; }
+
+        [JsonProperty("format")]
+        public string Format { get; set; }
+
+        [JsonProperty("timelength")]
+        public int Timelength { get; set; }
+
+        [JsonProperty("accept_format")]
+        public string AcceptFormat { get; set; }
+
+        [JsonProperty("accept_description")]
+        public List<string> AcceptDescription { get; set; }
+
+        [JsonProperty("accept_quality")]
+        public List<int> AcceptQuality { get; set; }
+
+        [JsonProperty("video_codecid")]
+        public int VideoCodecid { get; set; }
+
+        [JsonProperty("seek_param")]
+        public string SeekParam { get; set; }
+
+        [JsonProperty("seek_type")]
+        public string SeekType { get; set; }
+
+        [JsonProperty("dash")]
+        public Dash Dash { get; set; }
+
+        [JsonProperty("support_formats")]
+        public List<SupportFormat> SupportFormats { get; set; }
+    }
+
+    public class PlayerInfo
+    {
+        [JsonProperty("code")]
+        public int Code { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("ttl")]
+        public int Ttl { get; set; }
+
+        [JsonProperty("data")]
+        public Data Data { get; set; }
+
+        [JsonProperty("session")]
+        public string Session { get; set; }
+    }
+
+
+}
---
+++ BiliBili-dl/Program.cs
@@ -0,0 +1,221 @@
+﻿using HeyRed.Mime;
+using Newtonsoft.Json;
+using Penguin.Extensions.Strings;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace BiliBili_dl
+{
+    internal class Program
+    {
+        private const string PlayerInfoStart = "window.__playinfo__=";
+        private const string InitialStateStart = "window.__INITIAL_STATE__=";
+        private const string InitialStateEnd = ";(function(){var s;(s=document.currentScript||document.scripts[document.scripts.length-1]).parentNode.removeChild(s);}());";
+
+        private const string FFMPEG_PATH = "ffmpeg.exe";
+
+        private const string DownloadPath = "Downloads";
+
+        private const string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36";
+
+        private static readonly WebClient Client = new();
+
+        private static string Request(string url)
+        {
+            Client.Headers["User-Agent"] = UserAgent;
+
+            GZipStream responseStream = new(Client.OpenRead(url), CompressionMode.Decompress);
+            StreamReader reader = new(responseStream);
+            return reader.ReadToEnd();
+        }
+
+        private static async Task<string> DownloadBest(IEnumerable<Media> source, string title)
+        {
+            Console.WriteLine("Finding best media...");
+
+            Media bestMedia = source.OrderByDescending(a => a.Bandwidth).First();
+
+
+            string url = bestMedia.BaseUrl;
+            Console.WriteLine("Url: " + url);
+            Console.WriteLine("MimeType: " + bestMedia.MimeType);
+
+            string fileName = title;
+
+            string fileExt = $"{MimeTypesMap.GetExtension(bestMedia.MimeType)}";
+
+            Client.Headers["User-Agent"] = UserAgent;
+
+            if (!Directory.Exists(DownloadPath))
+            {
+                Directory.CreateDirectory(DownloadPath);
+            }
+
+            Console.WriteLine("Downloading media...");
+
+
+            string targetPath = Path.Combine(DownloadPath, $"{fileName}.{fileExt}");
+            int tryNum = 0;
+            while (File.Exists(targetPath))
+            {
+                tryNum++;
+
+                targetPath = Path.Combine(DownloadPath, $"{fileName} ({tryNum}).{fileExt}");
+            }
+
+            byte[] data = Array.Empty<byte>();
+
+            bool completed = false;
+
+            WebResponse response = null;
+
+            do
+            {
+                using MemoryStream ms = new();
+                
+                try
+                {
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+                    if(data.Length != 0)
+                    {
+                        request.AddRange(data.Length + 1, long.Parse(response.Headers["Content-Length"]));
+                    }
+
+                    request.Headers["User-Agent"] = UserAgent;
+
+                    response = await request.GetResponseAsync();
+
+                    using Stream stream = response.GetResponseStream();
+
+                    stream.CopyTo(ms);
+
+                    completed = true;
+
+                    response.Dispose();
+
+                }
+                catch (IOException ex) when (ex.Message.Contains("The response ended prematurely"))
+                {
+                    Console.WriteLine($"Connection dropped with {data.Length + ms.Length}/{response.Headers["Content-Length"]} bytes downloaded. Attempting continue...");
+                }
+
+                byte[] newData = new byte[data.Length + ms.Length];
+
+                int i = 0;
+
+                for (; i < data.Length; i++)
+                {
+                    newData[i] = data[i];
+                }
+
+                ms.Seek(0, SeekOrigin.Begin);
+
+                int b = 0;
+
+                while ((b = ms.ReadByte()) != -1)
+                {
+                    newData[i++] = (byte)b;
+                }
+
+                data = newData;
+
+            } while (!completed);
+
+            Console.WriteLine("Download complete. Writing file...");
+
+            File.WriteAllBytes(targetPath, data);
+
+            Console.WriteLine($"{bestMedia.MimeType} downloaded to: " + targetPath);
+
+            return new FileInfo(targetPath).FullName;
+        }
+
+        private static void MergeMedia(string audioPath, string videoPath)
+        {
+            Console.WriteLine("Merging audio and video...");
+
+            string tempOutput = Path.Combine(new FileInfo(videoPath).Directory.FullName, $"{Path.GetFileNameWithoutExtension(videoPath)}_Temp{Path.GetExtension(videoPath)}");
+            ProcessStartInfo startInfo = new(FFMPEG_PATH)
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+
+                Arguments = $"-i \"{videoPath}\" -i \"{audioPath}\" -c:v copy -c:a aac \"{tempOutput}\""
+            };
+
+            Process p = Process.Start(startInfo);
+
+            p.WaitForExit();
+
+            if (p.ExitCode != 0)
+            {
+                Console.WriteLine("Something went wrong during the merge.");
+                Console.WriteLine("Press any key to exit");
+                Environment.Exit(1);
+            }
+
+            Console.WriteLine("Cleaning up merge...");
+            File.Delete(audioPath);
+            File.Delete(videoPath);
+            File.Move(tempOutput, videoPath);
+        }
+
+        private static TReturn ExtractObject<TReturn>(string source, string start, string end = "</script>")
+        {
+            string jsonData = source.From(start).To(end);
+            Console.WriteLine("Parsed Json: " + jsonData);
+
+            return JsonConvert.DeserializeObject<TReturn>(jsonData);
+        }
+
+        private static async Task Main(string[] args)
+        {
+            foreach (string url in args)
+            {
+                Console.WriteLine("Checking playlist: " + url);
+
+                int page = 1;
+                int? maxPage = null;
+
+                string requestUrl = url;
+
+                do
+                {
+                    Console.WriteLine("Downloading source for: " + requestUrl);
+
+                    string pageSource = Request(requestUrl);
+
+                    InitialState initialState = ExtractObject<InitialState>(pageSource, InitialStateStart, InitialStateEnd);
+
+                    if (maxPage is null)
+                    {
+                        maxPage = initialState.VideoData.Pages.Count;
+                    }
+
+
+                    PlayerInfo playerInfo = ExtractObject<PlayerInfo>(pageSource, PlayerInfoStart);
+
+
+                    string title = initialState.VideoData.Pages[page - 1].Part;
+
+                    Task<string> audioFilePath = DownloadBest(playerInfo.Data.Dash.Audio, title);
+                    Task<string> videoFilePath = DownloadBest(playerInfo.Data.Dash.Video, title);
+
+
+                    MergeMedia(await audioFilePath, await videoFilePath);
+                    //MergeMedia("", await videoFilePath);
+                    page++;
+
+                    requestUrl = $"{url}?p={page}";
+
+                } while (maxPage != null && page <= maxPage);
+            }
+        }
+    }
+}
